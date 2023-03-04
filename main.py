d4f783a90a4b827f49843e5d0a87a6080ca2e2b5
hadala-p <100712141+hadala-p@users.noreply.github.com>
2023-03-04 17:11:11 +0100

    Added descriptions
    functions have been described and optimized

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from minesweeper import *
-from colorama import Fore, Back, Style
-
+# define minimum and maximum number of rows and columns
 min_rows = 8
 max_rows = 30
 min_columns = 8
@@ -8,15 +7,15 @@ max_columns = 24
 
 
 def sapper():
-    n_rows = 3
-    n_columns = 3
-    # n_rows = get_number(min_rows, max_rows, "Podaj liczbę wierszy")
-    # n_columns = get_number(min_columns, max_columns, "Podaj liczbę kolumn")
-    mines = lay_mines(int((n_rows * n_columns) * 0.17), n_rows, n_columns)
-    n_board = create_board(mines, n_rows, n_columns)
-    for i in range(n_rows):
-        print(n_board[i])
-    if reveal_fields(mines, n_board, n_rows, n_columns, set()) == 1:
+    n_rows = get_number(min_rows, max_rows, "Podaj liczbę wierszy")
+    n_columns = get_number(min_columns, max_columns, "Podaj liczbę kolumn")
+    mine_ratio = int((n_rows * n_columns) * 0.17)
+    mines = lay_mines(mine_ratio, n_rows, n_columns)
+    board = create_board(mines, n_rows, n_columns)
+    # for i in range(n_rows):
+    #     print(board[i])
+    points = set()
+    if reveal_fields(mines, board, points) == 1:
         print("Koniec gry, trafiono na bombe")
         return
     print("Wygrana, pozostaly tylko bomby!")
--- minesweeper.py
+++ minesweeper.py
@@ -1,3 +1,4 @@
+from colorama import Fore, Style  # Add a color library
 import random
 
 
@@ -18,15 +19,15 @@ def lay_mines(n_mines, rows, columns):
     return mines
 
 
-def create_board(n_mines, rows, columns, mine='*'):
+def create_board(mines, rows, columns, mine='*'):
     board = []
     for i in range(rows):
         row = []
         for j in range(columns):
-            if (i, j) in n_mines:
+            if (i, j) in mines:
                 row.append(mine)
             else:
-                row.append(number_of_neighbouring_mines(n_mines, i, j, rows, columns))
+                row.append(str(number_of_neighbouring_mines(mines, i, j, rows, columns)))
         board.append(row)
     return board
 
@@ -35,33 +36,43 @@ def number_of_neighbouring_mines(n_mines, x, y, rows, columns):
     neighboring_mines = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
-            if not (i == 0 and j == 0):
-                if (x + i, y + j) in n_mines:
-                    neighboring_mines += 1
-    return str(neighboring_mines)
+            if (x + i, y + j) in n_mines:
+                neighboring_mines += 1
+    return neighboring_mines
 
 
-def reveal_fields(n_mines, board, rows, columns, points):
-    x = get_number(0, rows-1, "Podaj x")
-    y = get_number(0, columns-1, "Podaj y")
+def reveal_fields(mines, board, points):
+    x = get_number(0, len(board) - 1, "Podaj x")
+    y = get_number(0, len(board[0]) - 1, "Podaj y")
     points.add((y, x))
-    print_board(board, rows, columns, points)
-    if len(points) == (rows * columns - len(n_mines)):
-        return
-    if (y, x) in n_mines:
+    print_board(board, points)
+
+    # check if the revealed square contains a mine
+    if (y, x) in mines:
         return 1
-    return reveal_fields(n_mines, board, rows, columns, points)
+    # check if all non-mine squares have been revealed
+    if len(points) == (len(board) * len(board[0]) - len(mines)):
+        return
+    # recursively reveal adjacent squares
+    return reveal_fields(mines, board, points)
 
 
-def print_board(board, rows, columns, points):
-    new_board = []
+def print_board(board, points):
+    rows = len(board)
+    columns = len(board[0])
+    # print column numbers
+    print("  ", end="")
+    for i in range(columns):
+        print(i, end=" ")
+    print()
+
+    # print row numbers and contents of each square
     for i in range(rows):
-        row = []
+        print(i, end=" ")
         for j in range(columns):
             if (i, j) in points:
-                row.append(board[i][j])
+                # if a square has been revealed, print its contents in yellow
+                print(Fore.YELLOW + board[i][j] + Style.RESET_ALL, end=" ")
             else:
-                row.append('X')
-        new_board.append(row)
-    for i in range(rows):
-        print(new_board[i])
+                print(Fore.BLUE + 'X' + Style.RESET_ALL, end=" ")
+        print()
